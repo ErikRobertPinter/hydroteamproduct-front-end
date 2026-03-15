59d4137744173468f45016cdeac964f0709d9ea8
Erik2013-P <erikpinter15@gmail.com>
2026-03-15 22:56:30 +0100

    feat:loginform added

--- src/app/public/login/login.ts
+++ src/app/public/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl(
+      '',
+      Validators.minLength(8),
+      Validators.maxLength(16),
+    ),
+  });
 
+  login() {}
 }
