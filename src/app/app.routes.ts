b1a1741b73027d76695be4f9cab92da6030d3f1d
Erik2013-P <erikpinter15@gmail.com>
2026-03-15 09:02:47 +0100

    feat:public routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './public/public-layout/public-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayout,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./public/home/home-module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'references',
+        loadChildren: () =>
+          import('./public/references/references-module').then(
+            (m) => m.ReferencesModule,
+          ),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('./public/about/about-module').then((m) => m.AboutModule),
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./public/login/login-module').then((m) => m.LoginModule),
+      },
+      {
+        path: 'contact',
+        loadChildren: () =>
+          import('./public/contact/contact-module').then(
+            (m) => m.ContactModule,
+          ),
+      },
+    ],
+  },
+];
