46f288dea8c09a01a4bb284c260ea91de2443a62
Erik2013-P <erikpinter15@gmail.com>
2026-03-15 09:28:09 +0100

    feat:register function basics added to authentication service

--- src/app/@shared/services/authentication.ts
+++ src/app/@shared/services/authentication.ts
@@ -1,12 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Authentication {
-  constructor(private http: HttpClient) {}
-  register() {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+  register() {
+    const dataLoad = 0;
+    return this.http.post('', dataLoad).subscribe({
+      next: () => {
+        this.router.navigate(['/successful-signup']);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 
   login() {}
 
